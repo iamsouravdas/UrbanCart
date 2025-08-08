bd7a077780b231471d046b8518c714f7612f291b
hp <hp@SOURAV>
2025-08-08 18:04:17 +0530

    UC-2: Access Token and Refresh Token added

--- src/routes/appRoute.ts
+++ src/routes/appRoute.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { UserController } from "../modules/user/contoller/UserController";
+import { verifyAccessToken } from "../middleware/verifyAccessToken";
 
 const appRoute = Router();
 const userController = new UserController();
 
 appRoute.post("/urban-cart/register", userController.register);
 appRoute.post("/urban-cart/login", userController.login);
-appRoute.get("/urban-cart/get-all-users", userController.getAllRegisteredUsers);
+appRoute.get("/urban-cart/get-all-users", verifyAccessToken, userController.getAllRegisteredUsers);
 
 
 
